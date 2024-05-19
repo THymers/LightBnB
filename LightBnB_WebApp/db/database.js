d2fc15776273a1fa91d1fb9165ae8ca07bff37e3
Tara <hymerstara@gmail.com>
2024-05-19 14:29:04 -0600

    feat:query for addProperty

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -8,16 +8,6 @@ const users = require("./json/users.json");
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-// const getUserWithEmail = function (email) {
-//   let resolvedUser = null;
-//   for (const userId in users) {
-//     const user = users[userId];
-//     if (user && user.email.toLowerCase() === email.toLowerCase()) {
-//       resolvedUser = user;
-//     }
-//   }
-//   return Promise.resolve(resolvedUser);
-// };
 const getUserWithEmail = function (email) {
   return pool
     .query(
@@ -42,9 +32,6 @@ const getUserWithEmail = function (email) {
  * @param {string} id The id of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-// const getUserWithId = function (id) {
-//   return Promise.resolve(users[id]);
-// };
 const getUserWithId = function (id) {
   return pool
     .query(
@@ -69,12 +56,6 @@ const getUserWithId = function (id) {
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-// const addUser = function (user) {
-//   const userId = Object.keys(users).length + 1;
-//   user.id = userId;
-//   users[userId] = user;
-//   return Promise.resolve(user);
-// };
 const addUser = function (user) {
   const { name, email, password } = user;
   return pool
@@ -102,10 +83,7 @@ const addUser = function (user) {
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-// const getAllReservations = function (guest_id, limit = 10) {
-//   return getAllProperties(null, 2);
-// };
-const getAllReservations = function (guest_id) {
+const getAllReservations = function (guest_id, limit = 10) {
   return pool
     .query(
       `
@@ -118,9 +96,9 @@ const getAllReservations = function (guest_id) {
     WHERE reservations.guest_id = $1
     GROUP BY reservations.id, properties.id
     ORDER BY reservations.start_date
-    LIMIT 10;
+    LIMIT $2;
   `,
-      [guest_id]
+      [guest_id, limit]
     )
     .then((result) => {
       return result.rows;
@@ -139,13 +117,6 @@ const getAllReservations = function (guest_id) {
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-// const getAllProperties = function (options, limit = 10) {
-//   const limitedProperties = {};
-//   for (let i = 1; i <= limit; i++) {
-//     limitedProperties[i] = properties[i];
-//   }
-//   return Promise.resolve(limitedProperties);
-// };
 
 const getAllProperties = (options, limit = 10) => {
   return pool
@@ -165,11 +136,70 @@ const getAllProperties = (options, limit = 10) => {
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+const addProperty = function(property) {
+  const {
+    owner_id,
+    title,
+    description,
+    thumbnail_photo_url,
+    cover_photo_url,
+    cost_per_night,
+    street,
+    city,
+    province,
+    post_code,
+    country,
+    parking_spaces,
+    number_of_bathrooms,
+    number_of_bedrooms
+  } = property;
+
+  return pool.query(
+    `
+    INSERT INTO properties (
+      owner_id,
+      title,
+      description,
+      thumbnail_photo_url,
+      cover_photo_url,
+      cost_per_night,
+      street,
+      city,
+      province,
+      post_code,
+      country,
+      parking_spaces,
+      number_of_bathrooms,
+      number_of_bedrooms
+    ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+    RETURNING *;
+  `,
+    [
+      owner_id,
+      title,
+      description,
+      thumbnail_photo_url,
+      cover_photo_url,
+      cost_per_night,
+      street,
+      city,
+      province,
+      post_code,
+      country,
+      parking_spaces,
+      number_of_bathrooms,
+      number_of_bedrooms
+    ]
+  )
+  .then((result) => {
+    return result.rows[0];
+  })
+  .catch((err) => {
+    console.error('Error executing query', err.stack);
+    throw err;
+  });
 };
+
 
 module.exports = {
   getUserWithEmail,
